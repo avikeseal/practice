ddfae96964c5ff5955b6b637583475538675f25b
Avike Seal <avikeseal@gmail.com>
2024-08-15 15:11:06 -0400

    everytime the loop repeats number gets a value that is 2 more than the previous value

--- js/patterns/loops.js
+++ js/patterns/loops.js
@@ -1,7 +1,13 @@
 //while and do loops:
 
 let number = 0;
+//everytime the loop repeats 
+//number gets a value more that is two more than it's 
+//previous value
 while (number <= 12) {
     console.log(number);
-    number = number + 12
+    //at the start of every repetition
+    //it is compared to 12 to decide whether
+    //the program's work is done
+    number += 2;
 }
